5478c1d8e5deda3aed35a1e7613b8c278e390382
federico <fraponi98@gmail.com>
2025-03-13 11:04:05 +0100

    fix palette

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import BaseContainer from "@/components/containers/base-container";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
     return (
         <BaseContainer>
             <div>
-                Ciao
+                <Button>
+                    Click me
+                </Button>
+                <Button
+                    variant={"secondary"}
+                >
+                    Click me
+                </Button>
             </div>
         </BaseContainer>
     );
